ac1f688a8b40c01aea990e72e5814231664510c3
hrp <543002649@qq.com>
2016-03-11 00:01:07 +0800

    创建新类： Timer

---
+++ eclipse/zscz/src/a/Timer.java
@@ -0,0 +1,29 @@
+/** @Title: Timer.java
+ * @Package a
+ * @Description: TODO
+ * @author 胡瑞鹏
+ * @date 2016年3月10日 下午11:55:30
+ * @version V1.0  
+ **/
+package a;
+
+/** @ClassName Timer
+ * @PackageName a
+ * @Description TODO
+ * @author 胡瑞鹏
+ * @Date 2016年3月10日 下午11:55:30
+ * @Version V1.0
+ */
+public class Timer {
+
+	/** @Title main
+	 * @Description TODO
+	 * @Author 胡瑞鹏
+	 * @CreateDate 2016年3月10日 下午11:55:31
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Helo git");
+	}
+
+}
